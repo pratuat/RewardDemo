88729eeb79446d7b598bd3e00b08b41e4cceb4fb
Pratuat Amatya <pratuat@gmail.com>
2019-08-04 23:23:46 +0200

    Test specs added and updated
    'event_processor_service_spec' updated. 'recommendation_service_spec' and 'user_service_spec' added.

--- spec/services/event_processor_service_spec.rb
+++ spec/services/event_processor_service_spec.rb
@@ -1,13 +1,28 @@
 require './spec/spec_helper'
 
 RSpec.describe EventProcessorService do
+    let(:event_params) { ["2018-06-14", "09:41", "B", "accepts"] }
     
-    context "UserCreationService" do
-        
+    subject { EventProcessorService.new(event_params, {}) }
+    
+    it 'should return date' do
+        expect(subject.event_date_time).to eq(DateTime.new(2018, 6, 14, 9, 41, 0))
     end
     
-    context "RecommendationService" do
+    context 'When event type is `accepts`' do
+        it 'should extend UserService' do
+            expect(subject.singleton_class.included_modules).to include(UserService)
+        end
+    end
+    
+    context 'When event type is `recommends`' do
+        let(:event_params) { ["2018-06-12", "09:41", "A", "recommends", "B"] }
         
+        subject { EventProcessorService.new(event_params, {}) }
+        
+        it 'should extend RecommendationService' do
+            expect(subject.singleton_class.included_modules).to include(RecommendationService,)
+        end
     end
 end
 
